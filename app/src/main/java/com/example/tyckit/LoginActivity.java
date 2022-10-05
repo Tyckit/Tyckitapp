e1f84a0c2c27a23647ce9b6d3c78413c49b57689
Tyckit <110340247+Tyckit@users.noreply.github.com>
2022-10-05 14:32:18 +0530

    Update LoginActivity.java
    Updated  with the existing code

--- app/src/main/java/com/example/tyckit/LoginActivity.java
+++ app/src/main/java/com/example/tyckit/LoginActivity.java
@@ -1,23 +1,33 @@
 package com.example.tyckit;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
-
+    TextView forgot_pwd;
     private FirebaseAuth mAuth;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         mAuth = FirebaseAuth.getInstance();
+        forgot_pwd = findViewById(R.id.forget_pwd);
+
         if (mAuth.getCurrentUser() != null) {
             finish();
             return;
@@ -25,6 +35,41 @@ public class LoginActivity extends AppCompatActivity {
 
         TextView btnLogin = findViewById(R.id.next_button);
         btnLogin.setOnClickListener(view -> authenticateUser());
+        forgot_pwd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText resetMail = new EditText((v.getContext()));
+                AlertDialog.Builder passwordReset = new AlertDialog.Builder(v.getContext());
+                passwordReset.setTitle("Reset Your Password");
+                passwordReset.setMessage("Enter email to receive the Reset link");
+                passwordReset.setView(resetMail);
+
+                passwordReset.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                            String mail = resetMail.getText().toString();
+                            mAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    //Toast.makeText("this","Reset mail has been sent");
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    //Toast.makeText("this","Reset mail is not sent");
+                                }
+                            });
+                    }
+                });
+                passwordReset.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                passwordReset.create().show();
+            }
+        });
 
         /*/
         TextView tvSwitchToRegister = findViewById(R.id.tvSwitchToRegister);
@@ -58,6 +103,8 @@ public class LoginActivity extends AppCompatActivity {
                                 Toast.LENGTH_SHORT).show();
                     }
                 });
+
+
     }
 
     @Override
